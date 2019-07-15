26af9f5fa294c50ec8734fb5a0cc9e0abbfdd08c
AlexYu01 <tengalexyu@gmail.com>
2019-07-15 14:33:43 -0700

    Calculate the product of two passed integers

---
+++ 0x03-more_functions_nested_loops/2-mul/2-main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int = 10, 71
+	fmt.Printf("The product of %d and %d is %d", a, b, mul(a, b))
+}
---
+++ 0x03-more_functions_nested_loops/2-mul/2-mul.go
@@ -0,0 +1,7 @@
+package main
+
+// mul Multiplies the two integers passed.
+// It returns the product.
+func mul(a, b int) int {
+	return (a * b)
+}
