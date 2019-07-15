6a50122092dc99e1178b56e415ee9db7c6044180
AlexYu01 <tengalexyu@gmail.com>
2019-07-15 14:35:06 -0700

    Perform FizzBuzz in Go

---
+++ 0x03-more_functions_nested_loops/6-fizz_buzz/6-fizz_buzz.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// main prints the fizz buzz challenge.
+func main() {
+	for i := 1; i < 100; i++ {
+		if i%15 == 0 {
+			fmt.Print("FizzBuzz ")
+		} else if i%3 == 0 {
+			fmt.Print("Fizz ")
+		} else if i%5 == 0 {
+			fmt.Print("Buzz ")
+		} else {
+			fmt.Printf("%d ", i)
+		}
+	}
+	fmt.Println("100")
+}
